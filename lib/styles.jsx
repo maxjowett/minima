7f1a7e16ca2aaf788e4d1c528ba01fe2bd0bde64
Max Jowett <jowett.max@gmail.com>
2020-07-11 22:55:45 -0700

    Add more generic styles

--- lib/styles.jsx
+++ lib/styles.jsx
@@ -2,6 +2,13 @@ import { css } from 'uebersicht';
 
 export const baseStyles = css`
   font-family: 'SF Mono', Menlo, monospace;
-  font-size: 13px;
+  color: #fafafa;
+  font-size: 14px;
   margin-top: 12px;
-`
+`;
+
+export const blockStyle = css`
+  background-color: #1a1a1a;
+  border-radius: 4px;
+  padding: 4px 8px;
+`;
